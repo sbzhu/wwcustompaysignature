53bdcbcff96cd5460519be317725b5dd204bb74d
sbzhu <abelzhu@tencent.com>
2018-11-07 17:47:29 +0800

    ok

--- php/signature.php
+++ php/signature.php
@@ -7,7 +7,46 @@ class SignatureGetter {
 		$this->secret = $secret;
 	}
 
-	public function GetSha1HmacSign(string $key, string $data)
+	public function Get(string $json)
+	{ 
+		$jsonArray = json_decode($json, true);
+
+		$paramArray = array();
+		$this->Json2Array($jsonArray, $paramArray);
+
+		$string = $this->ParamArray2String($paramArray);
+
+		return $this->HmacSha256($this->secret, $string);
+	}
+
+	private function ParamArray2String(array $array)
+	{
+		asort($array); // sort
+
+		//
+		$str = "";
+		foreach($array as $k => $v) { 
+			if (0 === strpos($v, 'sign=')) { // note: do not use ==, fuck !
+				continue; // escape sign self
+			}
+
+			$str = ($str . $v . '&');
+		} 
+		return substr($str, 0, strlen($str)-1);
+	}
+
+	private function Json2Array(array $jsonArray, array &$paramArray)
+	{ 
+		foreach($jsonArray as $k => $v) { 
+			if (is_array($v)) { 
+				$this->Json2Array($v, $paramArray);
+			} else { 
+				array_push($paramArray, $k . '=' . $v);
+			}
+		}
+	}
+
+	private function HmacSha256(string $key, string $data)
 	{
 		return base64_encode(hash_hmac("sha256", $data, $key, $raw_output = true));
 	}
--- php/test.php
+++ php/test.php
@@ -2,8 +2,26 @@
 include_once("signature.php");
 
 $secret = "gHqphoZuLCqHsSWbnojEKPLsWPE10G8UyKEE1B4uV64";
-$stringA = "errcode=0&errmsg=ok&nonce_str=5K8264ILTKCH16CQ2502SI8ZNMTM67VS&order_type=0&order_type=1&out_trade_no=1458098496971&out_trade_no=1458098496983&total_num=2&ts=1541498084";
+
+$json = '
+{
+	"errcode": 0,
+	"errmsg": "ok",
+	"total_num": 2,
+	"order_list": [
+	    	{
+			"out_trade_no": "1458098496971",
+			"order_type": 1
+		},
+		{
+			"out_trade_no": "1458098496983",
+			"order_type": 0
+		}
+	],
+	"ts": 1541498084,
+	"nonce_str": "5K8264ILTKCH16CQ2502SI8ZNMTM67VS",
+	"sign": "C380BEC2BFD727A4B6845133519F3AD6"
+} ';
 
 $signGetter = new SignatureGetter($secret); 
-
-print($signGetter->GetSha1HmacSign($secret, $stringA)."\n");
+print($signGetter->Get($json)."\n");

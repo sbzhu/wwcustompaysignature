047abc4cdce178d6ca842d4ef5a44c5673ca3935
abelzhu <abelzhu@tencent.com>
2018-11-08 10:46:57 +0800

    Update test.py

--- python/test.py
+++ python/test.py
@@ -13,8 +13,25 @@
  
 from signature import SignatureGetter
 
-secret = "aaaaaa"
-json = "bbb"
+secret = "gHqphoZuLCqHsSWbnojEKPLsWPE10G8UyKEE1B4uV64"
+json = """{
+   "errcode": 0,
+   "errmsg": "ok",
+   "total_num": 2,
+   "order_list": [
+        {
+     "out_trade_no": "1458098496971",
+     "order_type": 1
+    },
+    {
+     "out_trade_no": "1458098496983",
+     "order_type": 0
+    }
+   ],
+   "ts": 1541498084,
+   "nonce_str": "5K8264ILTKCH16CQ2502SI8ZNMTM67VS",
+   "sign": "C380BEC2BFD727A4B6845133519F3AD6"
+  }"""
 
 signGetter = SignatureGetter(secret)
 

7e7f6a86baa142d85434e4cef7dbf674d01444e7
abelzhu <abelzhu@tencent.com>
2018-11-08 10:47:26 +0800

    Update test.py

--- python/test.py
+++ python/test.py
@@ -32,6 +32,7 @@ json = """{
    "nonce_str": "5K8264ILTKCH16CQ2502SI8ZNMTM67VS",
    "sign": "C380BEC2BFD727A4B6845133519F3AD6"
   }"""
+stringA = "errcode=0&errmsg=ok&nonce_str=5K8264ILTKCH16CQ2502SI8ZNMTM67VS&order_type=0&order_type=1&out_trade_no=1458098496971&out_trade_no=1458098496983&total_num=2&ts=1541498084"
 
 signGetter = SignatureGetter(secret)
 
